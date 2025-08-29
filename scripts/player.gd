daf4e87abddfbd10d216239bbcd2377298dd9b98
Steve Giordano <steve1nyc@gmail.com>
2025-08-29 15:48:14 -0400

    explosion and sprites for spells (#2)
    Co-authored-by: Firoty1 <ultrapokegamer55@gmail.com>

--- scripts/player.gd
+++ scripts/player.gd
@@ -2,7 +2,7 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
-const AIR_RESISTANCE = 2
+const AIR_RESISTANCE = 4
 var gravity = 490
 
 var manaPower = 1000000
@@ -12,10 +12,10 @@ var stylePoints = 0
 
 const SPELLS = {
 	"airjet": {
-		"cost": 80
+		"cost": 8
 	},
 	"shark": {
-		"cost": 100, 
+		"cost": 10, 
 		"style": {
 			"dodge": 300,
 			"fail": -100,
@@ -23,11 +23,11 @@ const SPELLS = {
 		}
 	},
 	"explosion": {
-		"cost": 70,
+		"cost": 7,
 		"style": 100
 	},
 	"lightning": {
-		"cost": 100,
+		"cost": 10,
 		"style": {
 			"dodge": 300,
 			"fail": -100,
@@ -35,10 +35,10 @@ const SPELLS = {
 		}
 	},
 	"waterjet": {
-		"cost": 70
+		"cost": 7
 	},
 	"portal": {
-		"cost": 100,
+		"cost": 10,
 		"style": {
 			"dodge": 350,
 			"fail": -100,
@@ -46,10 +46,10 @@ const SPELLS = {
 		}
 	},
 	"leap": {
-		"cost": 80
+		"cost": 8
 	},
 	"flamingring": {
-		"cost": 110,
+		"cost": 11,
 		"style": {
 			"dodge": 0,
 			"fail": -150,
@@ -61,25 +61,77 @@ const SPELLS = {
 func castSpell(spell):
 	if (spell == 'airjet'):
 		if manaPower >= SPELLS.airjet.cost:
-			velocity.x = -2.5 * SPEED
+			velocity.x += -1.5 * SPEED
 			manaPower -= SPELLS.airjet.cost
 			$AnimatedSprite2D.animation = "AirJet"
+			$SpellEnd.start()
 	if (spell == 'leap'):
 		if manaPower >= SPELLS.leap.cost:
-			velocity.y = 1 * JUMP_VELOCITY
+			if velocity.y <= 0:
+				velocity.y += 1 * JUMP_VELOCITY
+			else:
+				velocity.y = 1 * JUMP_VELOCITY
+			$AnimatedSprite2D.play("Leap")
 			manaPower -= SPELLS.leap.cost
-			
+			$SpellEnd.start()
+	if (spell == 'waterjet'):
+		if manaPower >= SPELLS.waterjet.cost:
+			velocity.x += 1 * SPEED
+			manaPower -= SPELLS.waterjet.cost
+			$SpellEnd.start()
+	if (spell == 'explosion'):
+		if manaPower >= SPELLS.explosion.cost:
+			if velocity.y <= 0:
+				velocity.y += randf_range(1.25, 1.75) * JUMP_VELOCITY
+			else:
+				velocity.y = randf_range(1.25, 1.75) * JUMP_VELOCITY
+			velocity.x += randf_range(-2.75, -2.25) * SPEED
+			manaPower -= SPELLS.explosion.cost
+			$AnimatedSprite2D.play("Explosion")
+			$SpellEnd.start()
+	if (spell == 'portal'):
+		if manaPower >= SPELLS.portal.cost:
+			print("portal")
+			$AnimatedSprite2D.animation = "Portal"
+			$SpellEnd.start()
+	if (spell == 'lightning'):
+		if manaPower >= SPELLS.lightning.cost:
+			print("lightning")
+			$AnimatedSprite2D.animation = "Lightning"
+			$SpellEnd.start()
+	if (spell == 'shark'):
+		if manaPower >= SPELLS.shark.cost:
+			print("shark")
+			$AnimatedSprite2D.animation = "Shark"
+			$SpellEnd.start()
+	if (spell == 'flamingring'):
+		if manaPower >= SPELLS.flamingring.cost:
+			print("flamingring")
+			$AnimatedSprite2D.animation = "FlamingRing"
+			$SpellEnd.start()
 	ui.updateMana(manaPower)
-		
+
 func launch():
 	velocity.x = -3.5 * SPEED
 	velocity.y = 2 * JUMP_VELOCITY
-	
+	$AnimatedSprite2D.frame = 1
 func getInput():
 	if (Input.is_action_just_pressed("airjet")):
 		castSpell('airjet')
 	if (Input.is_action_just_pressed("leap")):
 		castSpell('leap')
+	if (Input.is_action_just_pressed("waterjet")):
+		castSpell('waterjet')
+	if (Input.is_action_just_pressed("explosion")):
+		castSpell('explosion')
+	if (Input.is_action_just_pressed("portal")):
+		castSpell('portal')
+	if (Input.is_action_just_pressed("lightning")):
+		castSpell('lightning')
+	if (Input.is_action_just_pressed("shark")):
+		castSpell('shark')
+	if (Input.is_action_just_pressed("flamingring")):
+		castSpell('flamingring')
 	if (Input.is_key_pressed(KEY_ENTER)):
 		launch()
 
@@ -89,14 +141,13 @@ func _physics_process(delta):
 		velocity.x += AIR_RESISTANCE
 	elif (velocity.x > 0):
 		velocity.x -= AIR_RESISTANCE
-		
-	# Add the gravity.
+	
 	if not is_on_floor():
 		velocity.y += gravity * delta
-
+	
 	getInput()
-	# Handle jump.
-	#if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		#velocity.y = JUMP_VELOCITY
-
 	move_and_slide()
+
+
+func _on_spell_end_timeout():
+	$AnimatedSprite2D.animation = "WizardBase"
